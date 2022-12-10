bd69e509d3e66c1c681e05b127e259a19642ef4d
BogdanPaul15 <paulbp68@gmail.com>
2022-12-10 22:38:47 +0200

    Fixed form

--- src/components/UI/UserDateForm/UserDateForm.jsx
+++ src/components/UI/UserDateForm/UserDateForm.jsx
@@ -3,24 +3,6 @@ import "./UserDateForm.scss";
 function UserDateForm(props) {
     return (
             <div>
-                <form className="form">
-                    <div className="formInputBox">
-                        <div className="formInputBlock">
-                            <div className="formInputLabel">
-                                <label htmlFor="firstname">Nume</label>
-                                <input type="text" className="firstname" name="firstname"></input>
-                            </div>
-                            <div className="formInputLabel">
-                                <label htmlFor="lastname">Prenume</label>
-                                <input type="text" className="lastname" name="lastname"></input>
-                            </div>
-                        </div>
-                        <div className="formInputLabel">
-                            <label htmlFor="email">Adresa de e-mail</label>
-                            <input type="email" className="email" name="email"></input>
-                        </div>
-                    </div>
-                </form>
                 <div className="formInputBox">
                     <div className="formInputBlock">
                         <div className="formInputLabel">
