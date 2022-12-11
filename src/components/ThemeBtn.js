986d3f2675f298ad220c4f7d8575d38c6b47cfe4
VladWero08 <vlad_wero@yahoo.com>
2022-12-11 10:29:36 +0200

    NewDarkMode

---
+++ src/components/ThemeBtn.js
@@ -0,0 +1,18 @@
+import React from "react";
+import useDarkMode from "use-dark-mode";
+import { useTheme } from "../services/useTheme";
+
+import "./ThemeBtn.scss";
+
+const ThemeBtn = () => {
+    const darkMode = useDarkMode(true);
+    const theme = useTheme();
+
+    return(
+        <button className="btn-theme" type="button" onClick={darkMode.toggle}>
+          {theme === "dark-mode" ? "Light mode" : "Dark mode"}
+        </button>
+    );
+};
+
+export default ThemeBtn;
--- src/components/UI/HeaderNavigation.jsx
+++ src/components/UI/HeaderNavigation.jsx
@@ -1,9 +1,37 @@
 import './HeaderNavigation.scss';
-
+import { useEffect} from "react";
+ 
 function HeaderNavigation(){
+    useEffect(() => {
+        var checkbox = document.querySelector('input[name=mode]');
+
+    checkbox.addEventListener('change', function() {
+        if(this.checked) {
+            trans()
+            document.documentElement.setAttribute('data-theme', 'dark')
+        } else {
+            trans()
+            document.documentElement.setAttribute('data-theme', 'light')
+        }
+    })
+
+    let trans = () => {
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('transition');
+        }, 1000)
+    }
+    },[]);
+    
+    
     return(
         <div className="header_navigation">
+             <div class="switch__btn">
+                <input class="container_toggle" type="checkbox" id="switch" name="mode"/>
+                <label for="switch">Toggle</label>
+            </div>
             <div>Programare vizita BCR</div>
+            <div></div>
         </div>
     )
 }
