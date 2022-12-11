268fdcf138d1704499d2bfb59e099f23fd828a88
BogdanPaul15 <paulbp68@gmail.com>
2022-12-11 06:30:22 +0200

    Responsive for pas1

--- src/components/SurveySteps/SurveyVisitScope.jsx
+++ src/components/SurveySteps/SurveyVisitScope.jsx
@@ -12,7 +12,7 @@ function SurveyVisitScope(){
             <MainTitle title="Despre ce vrei sa vorbim?"></MainTitle>
             <MainDescription desc="Alege unul dintre subiectele de mai jos, pentru care programezi vizita in unitate."></MainDescription>
             <div className="survey_unity__scope">
-                <DropDown></DropDown>
+                    <DropDown></DropDown>
                 <Buton title="Continua"></Buton>
             </div>
         </div>
