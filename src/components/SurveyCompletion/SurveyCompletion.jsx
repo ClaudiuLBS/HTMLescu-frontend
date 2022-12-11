4fdff564b74713e37fcf077d8a579dda1c02dc5b
VladWero08 <vlad_wero@yahoo.com>
2022-12-11 05:07:26 +0200

    created success page

---
+++ src/components/SurveyCompletion/SurveyCompletion.jsx
@@ -0,0 +1,22 @@
+import "./SurveyCompletion.scss";
+
+import HeaderNavigation from "../UI/HeaderNavigation";
+
+import VerificationIcon from "../../assets/images/verified-badge.png";
+// import DeleteIcon from "../../assets/images/trash.png";
+
+function SurveyCompletion(props){
+    return(
+        <div className="survey-completion__wrapper">
+            <HeaderNavigation></HeaderNavigation>
+            <div className="survey-completion__message">
+                <img src={VerificationIcon} alt="verification_icon" />
+                <h1>Vizita in uniunea BCR a fost programata cu succes!</h1>
+                <p>Urmeaza sa primesti pe adresa de email toate informatiile despre programarea ta.</p>
+            </div>
+            <a class="survey_back" href="https://www.bcr.ro/ro/persoane-fizice" target="_blank">Inapoi la bcr.ro</a>
+        </div>
+    )
+}
+
+export default SurveyCompletion;
